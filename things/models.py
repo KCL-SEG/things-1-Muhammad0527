a0d4e0c391b667d43f6d688293b3d6f06b42a7be
Muhammad Islam <muhammad.islam@kcl.ac.uk>
2023-10-18 15:00:41 +0100

    Add stuff

--- things/models.py
+++ things/models.py
@@ -6,4 +6,5 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.CharField(max_length=120, blank=True, unique=False)
     quantity = models.PositiveIntegerField(validators=
-                                           [MinValueValidator(0),MaxValueValidator(100)])
+                                           [MinValueValidator(0),MaxValueValidator(100)],
+                                           unique=False)
